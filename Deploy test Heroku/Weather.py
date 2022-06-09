9f9a5e7dc9c23483798919276ab6e31fbe876c22
GajAhmadaaa <naufaldi26@gmail.com>
2022-06-09 10:06:05 +0700

    Deploy test

---
+++ Deploy test Heroku/Weather.py
@@ -0,0 +1,59 @@
+# Import required library
+import requests
+from datetime import datetime
+today = datetime.today()
+
+# API Key for OpenWeatherMap API
+api_key = "81c713c1b0fea513c62681c97920daa9"
+
+# Function for defining root_url
+def root_url(url_param, city_name, api_key):
+    root_url = ["https://history.openweathermap.org/data/2.5/aggregated/month?", "https://api.openweathermap.org/data/2.5/weather?"]
+    root_url = root_url[url_param]
+    url = [f"{root_url}month={today.month}&q={city_name},ID&appid={api_key}", f"{root_url}q={city_name},ID&appid={api_key}"]
+    url = url[url_param]
+    r = requests.get(url)
+    data = r.json()
+    return r, data
+
+# Function for calling response from API
+def weather(city_name):
+    # Calling root_url function for history data
+    r, data = root_url(0, city_name, api_key)
+
+    # Checking if response data is 'code'
+    if 'cod' in data:
+        # Checking if the status of 'cod' is 200
+        if data['cod'] == 200:
+            # Getting the temperature from the json data
+            temp = data['result']['temp']['mean'] - 273.15
+            # Getting the humidity from the json data
+            humidity = data['result']['humidity']['mean']
+            # Getting rainfall data with equation
+            rainfall = data['result']['precipitation']['mean'] * 730.001
+
+    # Checking if response data is 'code'
+    elif 'code' in data:
+        # Checking if the status of 'code' is 404000
+        if data['code'] == 404000:
+            # Calling root_url function for current data
+            r, data = root_url(1, city_name, api_key)
+            # Checking if response data is 'cod'
+            if 'cod' in data:
+                # Checking if the status 'cod' is 200
+                if data['cod'] == 200:
+                    # Getting the temperature from the json data
+                    temp = data['main']['temp'] - 273.15
+                    # Getting the humidity from the json data
+                    humidity = data['main']['humidity']
+                    # Using average rainfall data in Indonesia
+                    rainfall = 235.51
+                else:
+                    return False # Kota tidak terdaftar
+
+            else:
+                return False # Nama kota tidak boleh kurang dari 3 karakter
+
+    temp = round(temp, 2)
+    rainfall = round(rainfall, 2)
+    return temp, humidity, rainfall
---
+++ Deploy test Heroku/app.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+from Weather import weather
+from flask import Flask, request
+
+app = Flask(__name__)
+
+df = pd.read_csv('dataset.csv')
+y = df.iloc[:,5].values
+encoder = LabelEncoder()
+encoder.fit_transform(y)
+
+reconstructed_model = tf.keras.models.load_model("model.h5")
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    args = request.args
+    city = args.get('city')
+    soil = int(args.get('soil'))
+    light = int(args.get('light'))
+    weathers = weather(city)
+    
+    if weathers is False:
+        return {"response": 404,
+                "message": "Nama kota salah atau tidak terdaftar"}
+    
+    else:
+        temp, humidity, rainfall = weathers
+        input = reconstructed_model.predict([[temp, soil, light, humidity, rainfall]])
+        predicted = np.argsort(input, axis=1)[:1]
+        dicts = {"response": 200}
+        for top_5 in predicted[:1,-5:]:
+            top_5 = encoder.inverse_transform(top_5)
+            i = 1
+            for name in top_5[::-1]:
+                dicts[str(i)] = name
+                i += 1
+                
+        return dicts
+
+if __name__ == "__main__":
+  app.run()
